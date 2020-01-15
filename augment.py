5f55bf5f662ce709a24561f9476617965b137296
Jon Gillick <jrgillick@gmail.com>
2020-01-14 16:07:04 -0800

    Fix augment fn

--- augment.py
+++ augment.py
@@ -31,29 +31,37 @@ def wav_augment(y, sr):
     return y
 
 def freq_mask(spec, F=30, num_masks=1, replace_with_zero=False):
-    cloned = copy.deepcopy(spec)#
-    num_mel_channels = cloned.shape[1]
+    # spec dimension is [num_frequency_channels, num_timesteps]
+    cloned = copy.deepcopy(spec)
+    num_mel_channels = cloned.shape[0]
+    num_timesteps = spec.shape[1]
     for i in range(0, num_masks):
         f = random.randrange(0, F)
         f_zero = random.randrange(0, num_mel_channels - f)
         # avoids randrange error if values are equal and range is empty
         if (f_zero == f_zero + f): return cloned
         mask_end = random.randrange(f_zero, f_zero + f)
-        if (replace_with_zero): cloned[0][f_zero:mask_end] = 0
-        else: cloned[0][f_zero:mask_end] = cloned.mean()
+        if replace_with_zero:
+            cloned[f_zero:mask_end,:] = 0
+        else:
+            cloned[f_zero:mask_end,:] = cloned.mean()
     return cloned
 
 def time_mask(spec, T=40, num_masks=1, replace_with_zero=False):
+    # spec dimension is [num_frequency_channels, num_timesteps]
     cloned = copy.deepcopy(spec)
-    len_spectro = cloned.shape[1]
+    num_mel_channels = cloned.shape[0]
+    num_timesteps = spec.shape[1]
     for i in range(0, num_masks):
         t = random.randrange(0, T)
-        t_zero = random.randrange(0, len_spectro - t)
+        t_zero = random.randrange(0, num_timesteps - t)
         # avoids randrange error if values are equal and range is empty
         if (t_zero == t_zero + t): return cloned
         mask_end = random.randrange(t_zero, t_zero + t)
-        if (replace_with_zero): cloned[0][:,t_zero:mask_end] = 0
-        else: cloned[:,t_zero:mask_end] = cloned.mean()
+        if replace_with_zero:
+            cloned[:,t_zero:mask_end] = 0
+        else:
+            cloned[:,t_zero:mask_end] = cloned.mean()
     return cloned
 
 def spec_augment(spec, prob=1.):
