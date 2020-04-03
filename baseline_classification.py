de766839cfc7eafab1b26d7a50ab4fbaaf7c9144
dz.luke <dz.luke@gmail.com>
2020-04-03 11:14:05 -0700

    add pitch class identification

--- baseline_classification.py
+++ baseline_classification.py
@@ -55,6 +55,8 @@ time = 4
 # number of samples between successive frames in librosa.melspectrogram
 mel_hop_length = 44100
 
+pitch_classes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
 
 def mix(fa, fb):
     diff = len(fa) - len(fb)
@@ -108,22 +110,70 @@ def calculate_features(sample, sr):
         feature = np.append(feature, padding)
     return feature
 
-def create_binary_label(instruments, orchestra):
+def extract_label(sample):
     '''
-        given a list of instruments, return a binary vector whose length is the number
-        of instruments in the orchestra
+        return the instrument name and pitch class given a file path
+        expects sample to be similar to "./TinySOL_0.6/TinySOL/BTb/BTb-ord-F2-ff.wav"
     '''
-    label = np.zeros(len(orchestra), dtype=np.float32)
-    for instrument in instruments:
-        index = orchestra.index(instrument)
+    sample = sample.split('/')[-1]
+    sample = sample.split('.')[0]
+    sample = sample.split('-')
+
+    instrument = sample[0]
+    playing_style = sample[1]
+    pitch = sample[2]
+    dynamic = sample[3]
+
+    pitch_class = pitch[:-1]
+    return [instrument, pitch_class]
+
+label_mapping = {}
+
+def create_label_mapping(orchestra, pitch_instruments):
+    '''
+        creates a dictionary that maps an instrument and pitch class to its index in the label
+
+        for instruments in 'pitch_instruments' a key looks like: Fl-C or Vn-G
+        for all other instruments, a key is just the instrument name: Fl or Vn or Cb
+    '''
+    assert len(label_mapping) == 0
+    i = 0
+    for instrument in pitch_instruments:
+        for pitch in pitch_classes:
+            key = instrument + '-' + pitch
+            label_mapping[key] = i
+            i += 1
+    for instrument in orchestra:
+        if instrument not in pitch_instruments:
+            label_mapping[instrument] = i
+            i += 1
+
+def create_binary_label(samples, pitch_instruments, orchestra):
+    '''
+        given a list of samples, return a binary vector
+
+        for N pitch_instruments, the first N * 12 indices correspond to each pitch_instrument and one of 12
+        pitches associated with it. The last indices are for instruments that aren't having pitches classified
+    '''
+    label_length = (len(pitch_instruments) * 12) + (len(orchestra) - len(pitch_instruments))
+    assert len(label_mapping) == label_length
+    label = np.zeros(label_length, dtype=np.float32)
+    for sample in samples:
+        instrument, pitch_class = extract_label(sample)
+        key = instrument
+        if instrument in pitch_instruments:
+            key = key + '-' + pitch_class
+        index = label_mapping[key]
         label[index] = 1.0
     return label
 
-def generate_data(orchestra, n, num_samples):
+def generate_data(orchestra, pitch_instruments, n, num_samples):
     '''
         create combinations of n instruments using only the instruments defined in 'orchestra'
+        the instruments in 'pitch_instruments' will have their pitch class indentified as well
 
     '''
+    create_label_mapping(orchestra, pitch_instruments)
 
     # dictionary where key is instrument name 
     # and value is a list of all the samples in the dataset for that instrument
@@ -141,6 +191,12 @@ def generate_data(orchestra, n, num_samples):
     for i in range(num_samples):
         # select n instruments
         instruments = random.sample(orchestra, n)
+
+        # ensure that at least 1 instrument from 'pitch_instruments' is in 'instruments'
+        if not any([instr in pitch_instruments for instr in instruments]):
+            instruments.pop()
+            instruments.append(random.choice(pitch_instruments))
+    
         samples_to_combine = []
         # for each of n chosen instruments, randomly select one sample
         for instrument in instruments:
@@ -151,8 +207,8 @@ def generate_data(orchestra, n, num_samples):
         mixture, sr = combine_sounds(samples_to_combine)
         # calculate feature of combined sound
         features = calculate_features(mixture, sr)
-        # create label from the instruments that were chosen
-        label = create_binary_label(instruments, orchestra)
+        # create label from the samples that were chosen
+        label = create_binary_label(samples_to_combine, pitch_instruments, orchestra)
 
         # since you cant know the dimensions until the features have been computed,
         # you can't make the ndarray until now
@@ -238,37 +294,6 @@ def make_plot(x, y):
     plt.show()
 
 
-orchestra = ['Vc', 'Fl', 'Va', 'Vn', 'Ob', 'BTb',
-               'Cb', 'ClBb', 'Hn', 'TpC', 'Bn', 'Tbn']
-
-
-
-# data = []
-# labels = []
-# num_samples = 50000
-# scores = []
-
-# for n in [1, 2, 3, 5, 10]:
-#     X, y = generate_data(orchestra, n, num_samples)
-
-#     score = train_and_test(X, y)[0]
-#     scores.append(score)
-
-#     print('---------------------------------')
-#     print("orchestra size: ", len(orchestra))
-#     print("n: ", n)
-#     print("number of samples: ", num_samples)
-#     print("scores from this run: ", score)
-#     print('---------------------------------')
-
-# print('*****************')
-# print("All training complete")
-# for n, score in zip([1, 2, 3, 5, 10], scores):
-#     print("n: {}, score: {}".format(n, score))
-# print('*****************')
-
-make_plot([1, 2, 3, 5, 10], [0.998, 0.554, 0.166, 0.043, 0.005])
-
 def make_plot_multiple_lines(all_scores):
     '''
     scores is a nested dictionary that maps a value of n to a dictionary of samples and scores
@@ -278,17 +303,15 @@ def make_plot_multiple_lines(all_scores):
         num_samples = list(scores.keys())
         accuracies = list(scores.values())
 
-        plt.plot(num_samples, accuracies, label="combinations of {} instruments".format(n), marker='o')
-
-        
+        plt.plot(num_samples, accuracies,
+                 label="combinations of {} instruments".format(n), marker='o')
 
         for x, y in scores.items():
             plt.annotate(str(y),  # this is the text
-                        (x, y),  # this is the point to label
-                        textcoords="offset points",  # how to position the text
-                        xytext=(0, 10),  # distance from text to points (x,y)
-                        ha='center')  # horizontal alignment can be left, right or center
-    
+                         (x, y),  # this is the point to label
+                         textcoords="offset points",  # how to position the text
+                         xytext=(0, 10),  # distance from text to points (x,y)
+                         ha='center')  # horizontal alignment can be left, right or center
 
     plt.ylim((0, 0.6))
     plt.legend()
@@ -298,12 +321,37 @@ def make_plot_multiple_lines(all_scores):
     plt.title("Accuracy of SVM classifying combinations of instruments".format(n))
     plt.show()
 
-# two = {1000: 0.09, 10000: 0.38, 30000: 0.51, 50000: 0.55}
-# three = {1000: 0.01, 10000: 0.117, 30000: 0.164, 50000: 0.193}
-# four = {1000: 0.0075, 10000: 0.047, 30000: 0.066, 50000: 0.075}
 
-# s = {2: two, 3: three, 4: four}
-# make_plot_multiple_lines(s)
+
+orchestra = ['Vc', 'Fl', 'Va', 'Vn', 'Ob', 'BTb',
+               'Cb', 'ClBb', 'Hn', 'TpC', 'Bn', 'Tbn']
+
+
+
+data = []
+labels = []
+num_samples = 50000
+scores = []
+n = 2
+
+pitch_instruments = ['Tbn', 'BTb']
+X, y = generate_data(orchestra, pitch_instruments, n, num_samples)
+
+score = train_and_test(X, y)[0]
+scores.append(score)
+
+print('---------------------------------')
+print("orchestra size: ", len(orchestra))
+print("n: ", n)
+print("number of samples: ", num_samples)
+print("scores from this run: ", score)
+print('---------------------------------')
+
+# print('*****************')
+# print("All training complete")
+# for n, score in zip([1, 2, 3, 5, 10], scores):
+#     print("n: {}, score: {}".format(n, score))
+# print('*****************')
 
 
 #eof
