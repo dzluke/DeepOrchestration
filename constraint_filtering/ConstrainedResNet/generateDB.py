7b65073c2d98530e34eddbc39fa6b9c19299d959
dz.luke <dz.luke@gmail.com>
2020-12-02 16:19:06 -0800

    Initial commit for ConstrainedResnet

---
+++ constraint_filtering/ConstrainedResNet/generateDB.py
@@ -0,0 +1,101 @@
+import os
+from functools import reduce
+import librosa
+import numpy as np
+import random
+
+from parameters import GLOBAL_PARAMS
+
+def getSampleMetaData(path,name):
+    s = {}
+    s['path'] = path
+    s['stft'] = {}
+    X = np.linspace((1-GLOBAL_PARAMS.coeff_freq_shift_data_augment)*GLOBAL_PARAMS.RATE, (1+GLOBAL_PARAMS.coeff_freq_shift_data_augment)*GLOBAL_PARAMS.RATE, 5)
+    for freq in X:
+        y,sr=librosa.load(path,sr=int(freq))
+        nb_samples = int(GLOBAL_PARAMS.TIME_LENGTH*GLOBAL_PARAMS.RATE)
+        if len(y) < nb_samples:
+            y = np.append(y, np.zeros((1,(nb_samples-len(y))), dtype=np.float32))
+        else:
+            y = y[:nb_samples]
+        s['stft'][int(freq)] = librosa.stft(y=y,hop_length=GLOBAL_PARAMS.MEL_HOP_LENGTH,n_fft=GLOBAL_PARAMS.N_FFT)
+    t = name.split('.')[0].split('-')
+    s['instrument'] = t[0]
+    s['style'] = t[1]
+    s['pitch_name'] = t[2]
+    s['nuance'] = t[3]
+    if len(t) == 5:
+        s['other'] = t[4]
+    
+    return s
+    
+
+def recursiveSearch(path, i=0):
+    """
+    create a list sample metadata dictionaries for each sample in path
+    @param path: path to database
+    @param i:
+    @return: list of sample metadata dictionaries
+    """
+    samples_metadata = []
+    s = i
+    for x in os.listdir(path):
+        p = os.path.join(path, x)
+        if os.path.isdir(p):
+            samples_metadata.extend(recursiveSearch(p, s))
+        elif os.path.isfile(p) and p.split('.')[-1] == 'wav':
+            print("Sample {}, Path: {}".format(s, p))
+            samples_metadata.append(getSampleMetaData(p, x))
+            s += 1
+    return samples_metadata
+
+def getPitchRange(d):
+    pr = set()
+    for i in d:
+        pr.add(i['pitch_name'])
+    pr = list(pr)
+    pr.sort(key = lambda x : 10000*ord(x[-1]) + 100*((ord(x[0])-ord('C'))%7) + (len(x)==3))
+    return pr
+
+
+def generateDBLabels(path, nb_pitch_range = 1, instr_filter=None):
+    '''
+        Generates the short term fourier transforms for each sample of the data base.
+        In order to ensure a good repartition of the pitches when generating the combinations,
+        the set is divided into pitch_bins (the total number of bins amond the pitch range is defined
+        by nb_pitch_range).
+        If instr_filter is not None, the generated set will contain only samples of the instruments
+        specified by instr_filter.
+        
+        Note that if the global paramaters MEL_HOP_LENGTH, N_FFT or TIME_LENGTH are modified,
+        this function needs to be called again
+    '''
+    samples = recursiveSearch(path)
+    # we sort the sample list so that the ordering of the samples is the same on every machine.
+    # otherwise, the indices in the labels could correspond to different samples
+    # because of differences in file structure on different machines
+    samples.sort(key=lambda s: s['path'])
+
+    if instr_filter is not None:
+        samples = [x for x in samples if x['instrument'] in instr_filter]
+    instr_db = {}
+    for i in samples:
+        if i["instrument"] not in instr_db:
+            instr_db[i["instrument"]] = []
+        instr_db[i["instrument"]].append(i)
+    pitch_range = getPitchRange(samples)
+    
+    database = {}
+    
+    nb_pitch_classes = nb_pitch_range or len(pitch_range)
+    for i in instr_db:
+        database[i] = [[] for i in range(nb_pitch_classes)]
+    for x in samples:
+        x['pitch_idx'] = pitch_range.index(x['pitch_name'])
+        database[x['instrument']][int(x['pitch_idx']*nb_pitch_classes/len(pitch_range))].append(x)
+        
+    for i in database:
+        database[i] = [x for x in database[i] if len(x) > 0]
+        for j in database[i]:
+            random.shuffle(j)
+    return database, pitch_range
---
+++ constraint_filtering/ConstrainedResNet/resnet.py
@@ -0,0 +1,159 @@
+import librosa
+import os
+import sys
+import pickle
+import time
+import librosa
+import torch
+import numpy as np
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from torch.distributions.categorical import Categorical
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+def count_parameters(model):
+    counts = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print('The model has {:,} trainable parameters'.format(counts))
+
+
+def init_weights(model):
+    for name, param in model.named_parameters():
+        nn.init.normal_(param.data, mean=0, std=0.01)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+
+        # Conv Layer 1
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels, out_channels=out_channels,
+            kernel_size=(3, 3), stride=stride, padding=1, bias=True
+        )
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        # Conv Layer 2
+        self.conv2 = nn.Conv2d(
+            in_channels=out_channels, out_channels=out_channels,
+            kernel_size=(3, 3), stride=1, padding=1, bias=True
+        )
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_channels=in_channels, out_channels=out_channels,
+                    kernel_size=(1, 1), stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = nn.ReLU()(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_classes, features_dim, dropout_rate=0.5, output_nonlinearity='sigmoid'):
+        super(ResNet, self).__init__()
+        # Initial input conv
+        shape_input = features_dim
+        self.conv1 = nn.Conv2d(
+            in_channels=1, out_channels=32, kernel_size=(3, 3),
+            stride=1, padding=1, bias=False
+        )
+
+        if output_nonlinearity is 'sigmoid':
+            self.output_nonlinearity = torch.sigmoid
+        elif output_nonlinearity is 'softmax':
+            self.output_nonlinearity = torch.softmax
+        else:
+            raise Exception("")
+
+        self.bn1 = nn.BatchNorm2d(32)
+
+        self.block1 = self._create_block(32, 32, stride=1)
+        self.block2 = self._create_block(32, 64, stride=2)
+        shape_input = ((shape_input[0]+1)//2, (shape_input[1]+1)//2)
+        self.block3 = self._create_block(64, 32, stride=2)
+        shape_input = ((shape_input[0]+1)//2, (shape_input[1]+1)//2)
+        self.block4 = self._create_block(32, 32, stride=2)
+        shape_input = ((shape_input[0]+1)//2, (shape_input[1]+1)//2)
+        
+        lin_input = 32*(shape_input[0]//2)*(shape_input[1]//2)
+        self.bn2 = nn.BatchNorm1d(lin_input)
+        self.linear1 = nn.Linear(lin_input, int(0.6*lin_input))
+        self.bn3 = nn.BatchNorm1d(int(0.6*lin_input))
+        self.linear2 = nn.Linear(int(0.6*lin_input), num_classes)
+
+        self.dropout = nn.Dropout(dropout_rate)
+
+    # A block is just two residual blocks for ResNet18
+    def _create_block(self, in_channels, out_channels, stride):
+        return nn.Sequential(
+            ResidualBlock(in_channels, out_channels, stride),
+            ResidualBlock(out_channels, out_channels, 1)
+        )
+
+    def forward(self, x):
+        out = nn.ReLU()(self.bn1(self.conv1(x)))
+        out = self.block1(out)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = nn.AvgPool2d(2)(out)
+        out = out.view(out.size(0), -1)
+        out = self.bn2(out)
+        out = self.dropout(out)
+        out = self.linear1(out)
+        out = self.bn3(out)
+        out = self.dropout(out)
+        out = F.relu(out)
+        out = self.linear2(out)
+        out = self.output_nonlinearity(out)
+        return out
+
+    def set_device(self, device):
+        for b in [self.block1, self.block2, self.block3, self.block4]:
+            b.to(device)
+        self.to(device)
+
+
+if __name__ == "__main__":
+    # Make a test input for the network and compute the features
+    sr = 44100  # sample rate
+    test_audio_signal = librosa.core.tone(1000, sr=sr, duration=4)
+
+    test_spec = librosa.feature.melspectrogram(test_audio_signal)
+    print("Melspec Feature Shape:")
+    print(test_spec.shape)
+
+
+    # Expected input size to the network is [batch_size, 128, 173]
+    batch_size = 32
+    batch_specs = np.tile(test_spec, (batch_size, 1, 1))
+    print("Batch Feature shape:")
+    print(batch_specs.shape)
+
+    # Network expects a channel dimension for the inputs, so add a dimension
+    test_tensor = torch.from_numpy(batch_specs).float().to(device)
+    test_tensor = test_tensor.unsqueeze(1)
+    print("Network input shape:")
+    print(test_tensor.shape)
+
+    resnet = ResNet(num_classes=505, dropout_rate=0.5)
+    init_weights(resnet)
+    resnet.set_device(device)
+
+    print("Network output shape:")
+    print(resnet(test_tensor).shape)
+
+    count_parameters(resnet)
