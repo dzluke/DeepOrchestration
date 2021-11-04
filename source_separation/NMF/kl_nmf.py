0a4476b322c0aa04a1c84595db3a7bb6bfa9150c
lched <68618979+lched@users.noreply.github.com>
2021-11-04 12:15:43 +0100

    Update NMF method

--- source_separation/NMF/kl_nmf.py
+++ source_separation/NMF/kl_nmf.py
@@ -1,13 +1,12 @@
 # kl_nmf.py
 # From https://github.com/CarmineCella/nmf_sources
-
 import numpy as np
+
 
 EPS = np.finfo(np.float32).eps  # as used in sklearn (nmf.py)
 
 def kl_nmf(X, k=10, it=100):
     # VERIFIED with sklearn.decomposition.nmf
-
     nr = X.shape[0]
     nc = X.shape[1]
 
@@ -20,20 +19,19 @@ def kl_nmf(X, k=10, it=100):
     np.abs(H, H)
 
     I = np.ones([nr,nc])
-    for i in range(it):
-        # update W
+    for _ in range(it):
+        # Update W
         WH = np.dot(W,H)
         WH[WH == 0] = EPS
-        N = np.dot(np.divide(X,WH),H.T)
-        P = np.dot(I,H.T)
+        N = np.dot(np.divide(X,WH), H.T)
+        P = np.dot(I, H.T)
         P[P == 0] = EPS
-        W *= np.divide(N,P)
-        #update H
+        W *= np.divide(N, P)
+        # Update H
         WH = np.dot(W,H)
         WH[WH == 0] = EPS
-        N = np.dot(W.T,np.divide(X,WH))
-        P = np.dot(W.T,I)
+        N = np.dot(W.T, np.divide(X, WH))
+        P = np.dot(W.T, I)
         P[P == 0] = EPS
-        H *= np.divide(N,P)
-
-    return W,H
+        H *= np.divide(N, P)
+    return W, H
--- source_separation/NMF/separate.py
+++ source_separation/NMF/separate.py
@@ -1,13 +1,11 @@
 # From https://github.com/CarmineCella/nmf_sources
 from pathlib import Path
 
-from librosa.core import fft
 import soundfile as sf
 import numpy as np
 import librosa as lr
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 
 from .kl_nmf import kl_nmf
 
@@ -25,14 +23,8 @@ def car2pol(sig):
     angle = np.arctan2(im, re)
     return amp, angle
 
-# Given polar coordinates, reconstruct the complex signal
-def pol2car(amp, angle):
-    re = amp * np.cos(angle)
-    im = amp * np.sin(angle)
-    return re + im*1j
 
-def get_sources (mix, nsources=4, components=32, dimensions=10, fft_size=4096,
-                 hoplen=512, phi_iter=0):
+def get_sources (mix, nsources=4, components=32, fft_size=4096, hoplen=512):
     # data preparation
     sources = np.zeros((nsources, len (mix)))
     specgram = lr.stft(mix, n_fft=fft_size, hop_length=hoplen);
@@ -42,43 +34,30 @@ def get_sources (mix, nsources=4, components=32, dimensions=10, fft_size=4096,
     [W, H] = kl_nmf(A, components)
 
     # masking
-    Wsum = np.sum(W, axis=1);
-    masks = np.zeros(W.shape);
-    for i in range(0, W.shape[1]):
-        masks[:, i] = W[:, i] / Wsum;
-        W[:, i] = W[:, i] * masks[:, i]
+    masks = np.zeros((A.shape[0],A.shape[1],COMPONENTS), dtype=complex)
+    comps = np.zeros(masks.shape, dtype=complex)
+    for i in range(0, COMPONENTS):
+        masks[:, :, i] = np.outer(W[:, i], H[i, :]) / np.dot(W, H)
+        comps[:, :, i] =  masks[:, :, i] * specgram
 
     # clustering
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(H)
-    if dimensions != 0:
-        pca = PCA(n_components=dimensions)
-        scaled_features = pca.fit_transform(scaled_features)
-    clusters = KMeans(n_clusters=nsources).fit (scaled_features)
+    clusters = KMeans (n_clusters=nsources).fit(scaled_features)
 
     # assignment and reconstruction
-    y_out = np.random.rand(len(mix))
     for s in range(0, nsources):
-        src = np.zeros(len(mix))
-
-        for k in range(0, components):
-            comp = np.zeros(len(mix))
+        comp = np.zeros(specgram.shape)
+        for k in range (0, components):
             if clusters.labels_[k] == s:
-                A1 = np.outer(W[:,k], H[k,:])
-                if phi_iter == 0 :
-                    specgramx = pol2car(A1, Phi)
-                    out = lr.istft(specgramx, hop_length=hoplen)
-                else:
-                    out = np.random.rand(len(mix))
-                    for _ in range (0, phi_iter):
-                        out_spec = lr.stft(out, n_fft=fft_size, hop_length=hoplen)
-                        specgramx = A1 * np.exp(complex(0.0, 1.0) * np.angle(out_spec))
-                        out = lr.istft(specgramx, hop_length=hoplen)
-                ml = min(len(src), len(out))
-                r = range(0, ml)
-                src[r] = src[r] + out[r]
-        sources[s] = src
+                comp = comp + comps[:, :, k]
+
+        src = lr.istft(comp, hop_length=hoplen)
+        ml = min(len(src), len(mix))
+        r = range(0, ml)
+        sources[s, r] = src[r]
     return W, H, clusters.labels_, sources
+
 
 def separate(in_path, out_path, verbose=False):
     out_path = Path(out_path)
@@ -87,8 +66,11 @@ def separate(in_path, out_path, verbose=False):
         print('total samples: ', len (mix))
         print('sources      : ', SOURCES)
         print('components   : ', COMPONENTS)
-    W, H, labels, sources = get_sources(mix, nsources=SOURCES, components=COMPONENTS,
-        dimensions=DIMENSIONS, fft_size=FFT_SIZE, hoplen=HOPLEN, phi_iter=PHI_ITER)
+    W, H, labels, sources = get_sources(mix,
+                                        nsources=SOURCES,
+                                        components=COMPONENTS,
+                                        fft_size=FFT_SIZE,
+                                        hoplen=HOPLEN)
     if verbose:
         print('W            : ', W.shape)
         print('H            : ', H.shape)
