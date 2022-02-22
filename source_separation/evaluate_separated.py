b4a1d1adc8681d43cf18cb3c8057b4bbe0a0d4ad
lched <leo.chedin@ens-paris-saclay.fr>
2022-02-22 17:42:44 +0100

    Update evaluate_separated.py

--- source_separation/evaluate_separated.py
+++ source_separation/evaluate_separated.py
@@ -1,19 +1,21 @@
-import os
 import argparse
-
-import numpy as np
+import csv
 import json
+import os
+
 import librosa
+import mir_eval
 import museval
+import numpy as np
 
-from pipeline import separation_models, SAMPLING_RATE
+from pipeline import SAMPLING_RATE, separation_models
 
 
-def main(targets_path, subtargets_path, separated_path, output_path):
+def main(metadata_path, subtargets_path, separated_path, outdir=None):
+
     # Get metadata
-    metadata_path = os.path.join(targets_path, 'metadata.json')
-    with open(metadata_path, 'r') as metadata_file:
-        metadata = json.load(metadata_file)
+    with open(metadata_path, 'r') as f:
+        metadata = json.load(f)
 
     # Initialize results dictionary.
     results = {}
@@ -21,8 +23,9 @@ def main(targets_path, subtargets_path, separated_path, output_path):
         results[model] = []
 
     # Evaluate each target
-    for target in metadata:
-        print('Evaluating results for {}'.format(target))
+    for idx, target in enumerate(metadata):
+        print('{}/{} Evaluating results for {}'.format(idx, len(metadata),
+                                                       target))
         # Get reference sources (= padded subtargets)
         target_metadata = metadata[target]
         ref_sources = []
@@ -30,8 +33,11 @@ def main(targets_path, subtargets_path, separated_path, output_path):
             audio, _ = librosa.load(os.path.join(subtargets_path,
                                                  data['name'] + '.wav'),
                                     sr=SAMPLING_RATE)
+            audio = librosa.util.normalize(audio)
             audio = np.pad(audio, data['padding'])
             ref_sources.append(audio)
+        ref_sources = np.array(ref_sources)
+        # ref_sources = ref_sources[..., np.newaxis]  #probably unnecessary
 
         # WORKAROUND (remove eventually)
         target = target.replace('*', '_')
@@ -41,30 +47,36 @@ def main(targets_path, subtargets_path, separated_path, output_path):
             # Get estimated sources(/subtargets)
             est_path = os.path.join(separated_path, model, target)
             est_sources_files = librosa.util.find_files(est_path)
-            if len(est_sources_files) != len(target_metadata):
-                raise ValueError('The number of estimated source should match'
-                                 'the number of references sources.')
+            assert len(est_sources_files) == len(target_metadata)
+
             est_sources = []
             for file in est_sources_files:
                 audio, _ = librosa.load(file, sr=SAMPLING_RATE)
+                audio = librosa.util.normalize(audio)
                 est_sources.append(audio)
+            est_sources = np.array(est_sources)
+            # est_sources = est_sources[..., np.newaxis]  #probably unnecessary
 
             (sdr, isr, sir, sar, perm) = museval.metrics.bss_eval(
-                ref_sources, est_sources, compute_permutation=True)
-            print('\t{}\tMean sdr: {}'.format(model, sdr.mean()))
+                ref_sources, est_sources, compute_permutation=True,
+                window=ref_sources.shape[1])
+            print('\t{}\t Mean sdr: {}'.format(model, np.nanmean(sdr)))
             results[model].append([target, sdr, sir, sar, perm])
 
-    if output_path:
-        # Save results (as pandas DataFrame I think)
-        pass
+    if outdir is not None:
+        with open(os.path.join(outdir, "results.csv"), 'w', newline='\n') as f:
+            wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+            wr.writerows(results)
+    return 0
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('targets_path', type=str)
+    parser.add_argument('metadata_file', type=str)
     parser.add_argument('subtargets_path', type=str)
     parser.add_argument('separated_path', type=str)
     parser.add_argument('-o', '--out_path', type=str, default=None,
                         dest='out_path')
     args, _ = parser.parse_known_args()
-    main(args.targets_path, args.subtargets_path, args.separated_path,
-         args.out_path)
+    main(args.metadata_file, args.subtargets_path, args.separated_path)
